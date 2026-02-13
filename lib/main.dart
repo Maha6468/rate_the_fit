b8a7a74d32f9307242e2c1129c9370bcfd141bd4
Maha6468 <motherma7633@gmail.com>
2026-02-13 23:58:21 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       //home: Home(),
       //home: Splash_Screen(),
       // home: Login(),
-      home: NewPostScreen(),
+      home: Create_Post(),
     );
   }
 }
