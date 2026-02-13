8eeba46ef84a5c669f7885f3121bb0ddf10ef582
Maha6468 <motherma7633@gmail.com>
2026-02-13 23:57:40 +0600

    last update

--- lib/Create_Post_Flow/create_post.dart
+++ lib/Create_Post_Flow/create_post.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import '../coustom_widget/custom_elevated_button.dart';
 
-class NewPostScreen extends StatefulWidget {
-  const NewPostScreen({super.key});
+class Create_Post extends StatefulWidget {
+  const Create_Post({super.key});
 
   @override
-  State<NewPostScreen> createState() => _NewPostScreenState();
+  State<Create_Post> createState() => _NewPostScreenState();
 }
 
-class _NewPostScreenState extends State<NewPostScreen> {
+class _NewPostScreenState extends State<Create_Post> {
+  File? selectedImage;
+  final ImagePicker _picker=ImagePicker();
+  Future<void> pickImage(ImageSource source) async {
+    final XFile? image = await _picker.pickImage(source: source);
+
+    if (image != null) {
+      setState(() {
+        selectedImage = File(image.path);
+      });
+    }
+  }
+
+
+
 
   List<String> tags = [
-    "Casual","Professional","Trendy","Classis",
-    "Sporty","Black","Red","Green"
+    "Casual",
+    "Professional",
+    "Trendy",
+    "Classis",
+    "Sporty",
+    "Black",
+    "Red",
+    "Green",
   ];
 
   List<String> selectedTags = [];
@@ -19,51 +42,137 @@ class _NewPostScreenState extends State<NewPostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color(0x000000),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
               Row(
                 children: [
-                   Icon(Icons.close, color: Colors.white),
-                   SizedBox(width: 10),
-                   Text("New Post", style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
-                  )
+                  Icon(Icons.close, color: Colors.white),
+                  SizedBox(width: 10),
+                  Text(
+                    "New Post",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
                 ],
               ),
 
-               SizedBox(height: 25),
+              SizedBox(height: 25),
 
-              Container(
-                height: 140,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  gradient: LinearGradient(
-                    colors: [
-                      //Color(0xff1a1a1a),
-                      //Color(0xff2a2a2a)
-                      Color(0xFF1C1914),
-                      Color(0xFF090C0E),
+              // Container(
+              //   height: 140,
+              //   width: double.infinity,
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(16),
+              //     gradient: LinearGradient(
+              //       colors: [
+              //         //Color(0xff1a1a1a),
+              //         //Color(0xff2a2a2a)
+              //         Color(0xFF1C1914),
+              //         Color(0xFF090C0E),
+              //       ],
+              //     ),
+              //   ),
+              //   child: Column(
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: [
+              //       Icon(Icons.upload_file, color: Colors.white, size: 28),
+              //       SizedBox(height: 8),
+              //       Text(
+              //         "Upload Photo",
+              //         style: TextStyle(
+              //           color: Colors.white,
+              //           fontWeight: FontWeight.bold,
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+
+
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    backgroundColor: Colors.black,
+                    builder: (_) {
+                      return SafeArea(
+                        child: Wrap(
+                          children: [
+                            ListTile(
+                              leading: const Icon(Icons.photo, color: Colors.white),
+                              title: const Text("Gallery",
+                                  style: TextStyle(color: Colors.white)),
+                              onTap: () {
+                                Navigator.pop(context);
+                                pickImage(ImageSource.gallery);
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.camera_alt, color: Colors.white),
+                              title: const Text("Camera",
+                                  style: TextStyle(color: Colors.white)),
+                              onTap: () {
+                                Navigator.pop(context);
+                                pickImage(ImageSource.camera);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  height: 140,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    gradient: const LinearGradient(
+                      colors: [
+                       // Color(0xff1a1a1a),
+                       // Color(0xff2a2a2a)
+                        Color(0xFF1C1914),
+                        Color(0xFF090C0E),
+                      ],
+                    ),
+                  ),
+                  child: selectedImage == null
+                      ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.upload_file, color: Colors.white, size: 28),
+                      SizedBox(height: 8),
+                      Text("Upload Photo",
+                          style: TextStyle(color: Colors.white70))
                     ],
+                  )
+                      : ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.file(
+                      selectedImage!,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    Icon(Icons.upload_file, color: Colors.white, size: 28),
-                    SizedBox(height: 8),
-                    Text("Upload Photo",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold))
-                  ],
-                ),
               ),
+
+
+
+
+
+
+
+
+
 
               SizedBox(height: 20),
 
@@ -100,17 +209,16 @@ class _NewPostScreenState extends State<NewPostScreen> {
                       });
                     },
                     child: Container(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 8),
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Colors.white12),
-                        color: isSelected
-                            ? Colors.white12
-                            : Colors.transparent,
+                        color: isSelected ? Colors.white12 : Colors.transparent,
                       ),
-                      child: Text(tag,
-                          style: const TextStyle(color: Colors.white)),
+                      child: Text(
+                        tag,
+                        style: const TextStyle(color: Colors.white),
+                      ),
                     ),
                   );
                 }).toList(),
@@ -119,41 +227,36 @@ class _NewPostScreenState extends State<NewPostScreen> {
               SizedBox(height: 30),
 
               Container(
-                padding:EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(14),
                   border: Border.all(color: Colors.white12),
                 ),
                 child: Row(
-                  children:[
-                    Icon(Icons.location_on_outlined,
-                        color: Colors.white70),
+                  children: [
+                    Icon(Icons.location_on_outlined, color: Colors.white70),
                     SizedBox(width: 8),
-                    Text("Tag location",
-                        style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold))
+                    Text(
+                      "Tag location",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ),
 
-               Spacer(),
+              Spacer(),
 
-              Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  gradient: const LinearGradient(
-                    colors: [Color(0xff3a2414), Color(0xff2b1a0f)],
-                  ),
-                ),
-                child: const Center(
-                  child: Text("Post",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600)),
-                ),
-              )
+              Custom_Elevated_Button(
+                text: 'Post',
+                width: 307,
+                height: 64,
+                onPressed: () {},
+                backgroundColor: Color(0xFF261C12),
+                circular_value: 60,
+              ),
             ],
           ),
         ),
