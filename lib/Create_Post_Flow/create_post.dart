64c1e020d6a2119cb61cd83cb3540d2a60642f08
Maha6468 <motherma7633@gmail.com>
2026-02-14 00:19:26 +0600

    last update

--- lib/Create_Post_Flow/create_post.dart
+++ lib/Create_Post_Flow/create_post.dart
@@ -12,14 +12,13 @@ class Create_Post extends StatefulWidget {
 
 class _NewPostScreenState extends State<Create_Post> {
   File? selectedImage;
-  final ImagePicker _picker=ImagePicker();
+  final ImagePicker _picker = ImagePicker();
+
   Future<void> pickImage(ImageSource source) async {
     final XFile? image = await _picker.pickImage(
-        source: source,
+      source: source,
       imageQuality: 70,
       maxWidth: 1080,
-
-
     );
 
     if (image != null) {
@@ -28,9 +27,6 @@ class _NewPostScreenState extends State<Create_Post> {
       });
     }
   }
-
-
-
 
   List<String> tags = [
     "Casual",
@@ -50,190 +46,200 @@ class _NewPostScreenState extends State<Create_Post> {
     return Scaffold(
       backgroundColor: Color(0x000000),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Icon(Icons.close, color: Colors.white),
-                  SizedBox(width: 10),
-                  Text(
-                    "New Post",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 25),
-
-              GestureDetector(
-                onTap: () {
-                  showModalBottomSheet(
-                    context: context,
-                    backgroundColor: Colors.black,
-                    builder: (_) {
-                      return SafeArea(
-                        child: Wrap(
-                          children: [
-                            ListTile(
-                              leading: const Icon(Icons.photo, color: Colors.white),
-                              title: const Text("Gallery",
-                                  style: TextStyle(color: Colors.white)),
-                              onTap: () {
-                                Navigator.pop(context);
-                                pickImage(ImageSource.gallery);
-                              },
-                            ),
-                            ListTile(
-                              leading: const Icon(Icons.camera_alt, color: Colors.white),
-                              title: const Text("Camera",
-                                  style: TextStyle(color: Colors.white)),
-                              onTap: () {
-                                Navigator.pop(context);
-                                pickImage(ImageSource.camera);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  height: 140,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    gradient: const LinearGradient(
-                      colors: [
-                       // Color(0xff1a1a1a),
-                       // Color(0xff2a2a2a)
-                        Color(0xFF1C1914),
-                        Color(0xFF090C0E),
-                      ],
-                    ),
-                  ),
-                  child: selectedImage == null
-                      ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.upload_file, color: Colors.white, size: 28),
-                      SizedBox(height: 8),
-                      Text("Upload Photo",
-                          style: TextStyle(color: Colors.white70))
-                    ],
-                  )
-                      : ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: Image.file(
-                      selectedImage!,
-                      fit: BoxFit.fill,
-                      width: double.infinity,
-                      height: double.infinity,
-                    ),
-                  ),
-                ),
-              ),
-
-
-
-
-
-
-
-
-
-
-              SizedBox(height: 20),
-
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  //color: Color(0xff1e1e1e),
-                  color: Color(0xFF1C1914),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextField(
-                  maxLines: 3,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "What Are you waring?",
-                    hintStyle: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25),
-
-              Wrap(
-                spacing: 10,
-                runSpacing: 15,
-                children: tags.map((tag) {
-                  final isSelected = selectedTags.contains(tag);
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isSelected
-                            ? selectedTags.remove(tag)
-                            : selectedTags.add(tag);
-                      });
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.white12),
-                        color: isSelected ? Colors.white12 : Colors.transparent,
-                      ),
-                      child: Text(
-                        tag,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
-
-              SizedBox(height: 30),
-
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14),
-                  border: Border.all(color: Colors.white12),
-                ),
-                child: Row(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Icon(Icons.location_on_outlined, color: Colors.white70),
-                    SizedBox(width: 8),
+                    Icon(Icons.close, color: Colors.white),
+                    SizedBox(width: 10),
                     Text(
-                      "Tag location",
+                      "New Post",
                       style: TextStyle(
                         color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ],
                 ),
-              ),
-
-              Spacer(),
-
-              Custom_Elevated_Button(
-                text: 'Post',
-                width: 307,
-                height: 64,
-                onPressed: () {},
-                backgroundColor: Color(0xFF261C12),
-                circular_value: 60,
-              ),
-            ],
+          
+                SizedBox(height: 25),
+          
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      backgroundColor: Colors.black,
+                      builder: (_) {
+                        return SafeArea(
+                          child: Wrap(
+                            children: [
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.photo,
+                                  color: Colors.white,
+                                ),
+                                title: const Text(
+                                  "Gallery",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  pickImage(ImageSource.gallery);
+                                },
+                              ),
+                              ListTile(
+                                leading: const Icon(
+                                  Icons.camera_alt,
+                                  color: Colors.white,
+                                ),
+                                title: const Text(
+                                  "Camera",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  pickImage(ImageSource.camera);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: Container(
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      gradient: const LinearGradient(
+                        colors: [
+                          // Color(0xff1a1a1a),
+                          // Color(0xff2a2a2a)
+                          Color(0xFF1C1914),
+                          Color(0xFF090C0E),
+                        ],
+                      ),
+                    ),
+                    child: selectedImage == null
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.upload_file,
+                                color: Colors.white,
+                                size: 28,
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                "Upload Photo",
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                            ],
+                          )
+                        : ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.file(
+                              selectedImage!,
+                              fit: BoxFit.fill,
+                              width: double.infinity,
+                              height: double.infinity,
+                            ),
+                          ),
+                  ),
+                ),
+          
+                SizedBox(height: 20),
+          
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    //color: Color(0xff1e1e1e),
+                    color: Color(0xFF1C1914),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    maxLines: 3,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "What Are you waring?",
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+          
+                Wrap(
+                  spacing: 10,
+                  runSpacing: 15,
+                  children: tags.map((tag) {
+                    final isSelected = selectedTags.contains(tag);
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isSelected
+                              ? selectedTags.remove(tag)
+                              : selectedTags.add(tag);
+                        });
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(color: Colors.white12),
+                          color: isSelected ? Colors.white12 : Colors.transparent,
+                        ),
+                        child: Text(
+                          tag,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+          
+                SizedBox(height: 30),
+          
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    border: Border.all(color: Colors.white12),
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.location_on_outlined, color: Colors.white70),
+                      SizedBox(width: 8),
+                      Text(
+                        "Tag location",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //Spacer(),
+          SizedBox(height: 80,),
+                Center(
+                  child: Custom_Elevated_Button(
+                    text: 'Post',
+                    width: 307,
+                    height: 64,
+                    onPressed: () {},
+                    backgroundColor: Color(0xFF261C12),
+                    circular_value: 60,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
