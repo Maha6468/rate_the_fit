a330f87cf72e60303da949a6b7cc79ff47e3bb8a
Maha6468 <motherma7633@gmail.com>
2026-02-15 00:57:11 +0600

    last update

--- lib/message_flow/Chat_1.dart
+++ lib/message_flow/Chat_1.dart
@@ -8,27 +8,27 @@ class MessagesScreen extends StatelessWidget {
     {
       "name": "Brooklyn Simmons",
       "message": "Where did you get that from?",
-      "image": "https://randomuser.me/api/portraits/women/1.jpg"
+      "image": "https://randomuser.me/api/portraits/women/1.jpg",
     },
     {
       "name": "Darrell Steward",
       "message": "Where did you get that from?",
-      "image": "https://randomuser.me/api/portraits/men/2.jpg"
+      "image": "https://randomuser.me/api/portraits/men/2.jpg",
     },
     {
       "name": "Courtney Henry",
       "message": "Where did you get that from?",
-      "image": "https://randomuser.me/api/portraits/women/3.jpg"
+      "image": "https://randomuser.me/api/portraits/women/3.jpg",
     },
     {
       "name": "Jacob Jones",
       "message": "Where did you get that from?",
-      "image": "https://randomuser.me/api/portraits/men/4.jpg"
+      "image": "https://randomuser.me/api/portraits/men/4.jpg",
     },
     {
       "name": "Dianne Russell",
       "message": "Where did you get that from?",
-      "image": "https://randomuser.me/api/portraits/women/5.jpg"
+      "image": "https://randomuser.me/api/portraits/women/5.jpg",
     },
   ];
 
@@ -38,10 +38,10 @@ class MessagesScreen extends StatelessWidget {
       backgroundColor: Color(0x000000),
 
       appBar: AppBar(
-        title: const Text("Message",style: TextStyle(color: Colors.white),),
+        title: const Text("Message", style: TextStyle(color: Colors.white)),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,color: Colors.white,),
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {},
         ),
         backgroundColor: Colors.black,
@@ -58,31 +58,32 @@ class MessagesScreen extends StatelessWidget {
                     decoration: InputDecoration(
                       hintText: "Search.....",
                       hintStyle: TextStyle(color: Colors.white),
-                      prefixIcon: const Icon(Icons.search,size: 30,color: Colors.white,),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                       filled: true,
                       fillColor: Color(0x000000),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12),
                         borderSide: BorderSide(
                           //border: Border.all(color: Colors.white12),
-                          color: Colors.white12
-                        )
+                          color: Colors.white12,
+                        ),
                       ),
                     ),
                   ),
                 ),
-                SizedBox(width: 18,),
+                SizedBox(width: 18),
                 Container(
                   padding: EdgeInsets.all(12),
                   decoration: BoxDecoration(
                     color: Color(0x000000),
                     borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.white12)
+                    border: Border.all(color: Colors.white12),
                   ),
-                  child: const Icon(
-                    Icons.filter_list,
-                    color: Colors.white,
-                  ),
+                  child: const Icon(Icons.filter_list, color: Colors.white),
                 ),
               ],
             ),
@@ -96,7 +97,7 @@ class MessagesScreen extends StatelessWidget {
                     color: Color(0x000000),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
-                        side: BorderSide( color: Colors.white12)
+                      side: BorderSide(color: Colors.white12),
                     ),
                     margin: EdgeInsets.symmetric(vertical: 10),
                     child: ListTile(
@@ -110,14 +111,12 @@ class MessagesScreen extends StatelessWidget {
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 18,
-                          color: Colors.white
+                          color: Colors.white,
                         ),
                       ),
                       subtitle: Text(
                         chat["message"]!,
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
+                        style: TextStyle(color: Colors.grey),
                       ),
                       trailing: Container(
                         padding: EdgeInsets.all(8),
@@ -142,32 +141,38 @@ class MessagesScreen extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.orange,
+      floatingActionButton: SizedBox(
+        width: 70,height: 70,
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add,size: 35,color: Colors.white,),
+          backgroundColor: Colors.grey.shade800,
+          shape: CircleBorder(),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
-        shape: const CircularNotchedRectangle(),
+        color: Colors.grey.shade800,
+        //color: Colors.amberAccent,
+        shape: CircularNotchedRectangle(
+
+        ),
         notchMargin: 6,
+        elevation: 10,
         child: SizedBox(
           height: 60,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: const [
-              Icon(Icons.home, color: Colors.white),
-              Icon(Icons.star, color: Colors.white),
-              SizedBox(width: 48), // Space for FAB
-              Icon(Icons.chat_bubble, color: Colors.white),
-              Icon(Icons.person, color: Colors.white),
+              Icon(Icons.home, color: Colors.white,size: 30,),
+              Icon(Icons.star, color: Colors.white,size: 30,),
+              SizedBox(width: 48),
+              Icon(Icons.chat_bubble, color: Colors.white,size: 30,),
+              Icon(Icons.person, color: Colors.white,size: 30,),
             ],
           ),
         ),
       ),
-
-
     );
   }
 }
