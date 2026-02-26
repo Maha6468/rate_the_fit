4f3071c9957a5ec436441da9a253a7869ecebe85
Maha6468 <motherma7633@gmail.com>
2026-02-26 07:29:07 +0600

    last update

--- lib/message_flow/chat_inner.dart
+++ lib/message_flow/chat_inner.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:record/record.dart';
 
 class Chat_Inner extends StatefulWidget {
   const Chat_Inner({super.key});
@@ -17,7 +22,6 @@ class _Chat_InnerState extends State<Chat_Inner> {
       body: Column(
         children: [
           Expanded(child: ChatList()),
-
           MessageInputBar(),
         ],
       ),
@@ -149,39 +153,130 @@ class ChatBubble extends StatelessWidget {
   }
 }
 
-class MessageInputBar extends StatelessWidget {
+class MessageInputBar extends StatefulWidget {
   const MessageInputBar({super.key});
+
+  @override
+  State<MessageInputBar> createState() => _MessageInputBarState();
+}
+
+class _MessageInputBarState extends State<MessageInputBar> {
+  final ImagePicker _picker=ImagePicker();
+  File? _image;
+
+  Future<void> openCamera() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (photo != null) {
+      setState(() {
+        _image = File(photo.path);
+      });
+    }
+  }
+
+
+  final AudioRecorder _recorder=AudioRecorder();
+  bool _isRecording=false;
+  String? _filePath;
+
+  Future<void> _startRecording() async {
+    if (await _recorder.hasPermission()) {
+      final dir = await getApplicationDocumentsDirectory();
+
+      _filePath = '${dir.path}/voice_${DateTime.now().millisecondsSinceEpoch}.m4a';
+
+      await _recorder.start(
+        const RecordConfig(),
+        path: _filePath!,
+      );
+
+      setState(() => _isRecording = true);
+    }
+  }
+
+  Future<void> _stopRecording() async {
+    await _recorder.stop();
+    setState(() => _isRecording = false);
+
+    print("Saved at: $_filePath"); // এই file পরে chat এ পাঠাতে পারবা
+  }
+
+  @override
+  void dispose() {
+    _recorder.dispose();
+    super.dispose();
+  }
+
+  Future<void> openGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      setState(() {
+        _image = File(image.path);
+      });
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        height: 70,
-        decoration: BoxDecoration(
-          color: const Color(0xff2a201a),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Row(
-          children: [
-            const Icon(Icons.camera_alt, color: Colors.white70, size: 30),
-            const SizedBox(width: 10),
-            const Expanded(
-              child: TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: "Type Your message here.....",
-                  hintStyle: TextStyle(color: Colors.white54),
-                  border: InputBorder.none,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (_image != null)
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.file(
+                  _image!,
+                  height: 80,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-            const Icon(Icons.mic, color: Colors.white70, size: 30),
-            const SizedBox(width: 10),
-            const Icon(Icons.image_outlined, color: Colors.white70, size: 30),
-          ],
-        ),
+
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            height: 70,
+            decoration: BoxDecoration(
+              color: const Color(0xff2a201a),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: [
+                IconButton(onPressed:openCamera, icon: Icon(Icons.camera_alt, color: Colors.white70, size: 30)),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: "Type Your message here.....",
+                      hintStyle: TextStyle(color: Colors.white54),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: _isRecording ? _stopRecording : _startRecording,
+                  icon: Icon(
+                    _isRecording ? Icons.stop : Icons.mic,
+                    color: _isRecording ? Colors.red : Colors.white70,
+                    size: 30,
+                  ),
+                ),
+                 SizedBox(width: 10),
+                 //Icon(Icons.image_outlined, color: Colors.white70, size: 30),
+                IconButton(
+                  onPressed: openGallery,
+                  icon: const Icon(Icons.image_outlined, color: Colors.white70, size: 30),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
