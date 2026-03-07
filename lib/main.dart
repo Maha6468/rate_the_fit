4e97078dbc6d98d26b0e56e3aa518489f45f75da
Maha6468 <motherma7633@gmail.com>
2026-03-07 12:16:30 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rate_the_fit/practice.dart';
 import 'package:rate_the_fit/screen/Profile_Flow/Someone_else.dart';
+import 'package:rate_the_fit/screen/Sign_Up/Sign_up.dart';
 import 'package:rate_the_fit/screen/home_flow/search.dart';
 import 'package:rate_the_fit/screen/rating_flow/Rating_Flow_1.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/account_settings.dart';
@@ -9,7 +10,7 @@ import 'package:rate_the_fit/screen/Side_Bar_Flow/notification.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/notification_settings.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/preferences_2.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/privacy_&_policy.dart';
-import 'package:rate_the_fit/screen/Side_Bar_Flow/privacy_settings.dart';
+import 'package:rate_the_fit/screen/Side_Bar_Flow/privacy_settings_2.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/security_settings.dart';
 import 'package:rate_the_fit/screen/home_flow/add_to_story.dart';
 import 'package:rate_the_fit/screen/message_flow/chat_inner.dart';
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
       //home: Home(),
       //home: Splash_Screen(),
       // home: Login(),
+      //home: Sign_Up (),
       //home: Create_Post(),
       //home: MessageListScreen(),
       //home: Chat_1(),
@@ -53,16 +55,15 @@ class MyApp extends StatelessWidget {
       //home: Notification_Settings(),
       //home: Notifications(),
       //home: Preferences_2(),
-      //home: Privacy_Settings(),
-      //home:PrivacySettingsPage(),
-      //home: Security_Settings(),
+      //home: Privacy_Settings_2(),
+      home: Security_Settings_2(),
       //home: Faq_Screen(),
       //home: PrivacyPolicyScreen(),
-     // home: ProfileScreen(),
+      //home: Someone_else(),
         //home: Add_To_Story(),
        // home: Rating_Home_1(),
       //home: Rating_Home_2(),
-        home: Search_1(),
+       // home: Search_1(),
     );
   }
 }
