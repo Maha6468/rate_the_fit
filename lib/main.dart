3d89af7bcc61cda8df73c7e5587181cf8bc64cd3
Maha6468 <motherma7633@gmail.com>
2026-03-07 12:27:13 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,6 @@ import 'screen/Side_Bar_Flow/side_bar.dart';
 import 'screen/message_flow/Chat_1.dart';
 import 'screen/message_flow/chat_2.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -35,11 +33,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rate The Fit',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Splash_Screen(),
+        "/home": (context) => Home(),
+        "/signup": (context) => Sign_Up(),
+        "/createPost": (context) => Create_Post(),
+        "/chat1": (context) => Chat_1(),
+        "/chat2": (context) => Chat_2(),
+        "/chatInner": (context) => Chat_Inner(),
+        "/sidebar": (context) => CustomDrawer(),
+        "/accountSettings": (context) => Account_Settings(),
+        "/notificationSettings": (context) => Notification_Settings(),
+        "/notifications": (context) => Notifications(),
+        "/preferences": (context) => Preferences_2(),
+        "/privacySettings": (context) => Privacy_Settings_2(),
+        "/securitySettings": (context) => Security_Settings_2(),
+        "/faq": (context) => Faq_Screen(),
+        "/privacyPolicy": (context) => PrivacyPolicyScreen(),
+        "/someoneElse": (context) => Someone_else(),
+        "/addStory": (context) => Add_To_Story(),
+        "/rating1": (context) => Rating_Home_1(),
+        "/rating2": (context) => Rating_Home_2(),
+        "/search": (context) => Search_1(),
+      },
+
       //home: Home(),
       //home: Splash_Screen(),
       // home: Login(),
@@ -51,19 +74,19 @@ class MyApp extends StatelessWidget {
       //home: Chat_Inner(),
       //home: CustomDrawer(),
       //home:CustomDrawer()
-     // home: Account_Settings(),
+      // home: Account_Settings(),
       //home: Notification_Settings(),
       //home: Notifications(),
       //home: Preferences_2(),
       //home: Privacy_Settings_2(),
-      home: Security_Settings_2(),
+      //home: Security_Settings_2(),
       //home: Faq_Screen(),
       //home: PrivacyPolicyScreen(),
       //home: Someone_else(),
-        //home: Add_To_Story(),
-       // home: Rating_Home_1(),
+      //home: Add_To_Story(),
+      // home: Rating_Home_1(),
       //home: Rating_Home_2(),
-       // home: Search_1(),
+      // home: Search_1(),
     );
   }
 }
