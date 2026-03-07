a41091b9c7c8f75d0b141f0beaf61d05854955e8
Maha6468 <motherma7633@gmail.com>
2026-03-07 23:27:16 +0600

    ..................................................................................

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:rate_the_fit/screen/log_In/Login.dart';
-
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({super.key});
@@ -12,64 +10,64 @@ class Splash_Screen extends StatefulWidget {
 }
 
 class _Splash_ScreenState extends State<Splash_Screen> {
-
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 10),(){
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Login()));
-    }
-    );
+    Timer(Duration(seconds: 10), () {
+      Navigator.pushReplacementNamed(
+        context, "/login"
+      );
+    });
   }
+
   @override
-
-
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Colors.black87,
-                  Color(0xFF1C1914),
-                  Color(0xFF090C0E)],
-                begin: Alignment.bottomLeft,
-                end: Alignment.topLeft)),
+          gradient: LinearGradient(
+            colors: [Colors.black87, Color(0xFF1C1914), Color(0xFF090C0E)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topLeft,
+          ),
+        ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black54,
-                        blurRadius: 15,
-                        spreadRadius: 2,
-                        offset: Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: Image.asset(
-                    "assets/images/star.png",
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.cover,
-                  )),
-              SizedBox(
-                height: 30,
+                padding: EdgeInsets.all(width * 0.05),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      blurRadius: 15,
+                      spreadRadius: 2,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Image.asset(
+                  "assets/images/star.png",
+                  //height: 100,
+                  //width: 100,
+                  height: height * 0.15,
+                  width: width * 0.30,
+                  fit: BoxFit.contain,
+                ),
               ),
+              SizedBox(height: height * 0.04),
               Text(
                 "Rate The Fit App",
                 style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Color(0xFFFFF4C7)),
+                  fontWeight: FontWeight.bold,
+                  fontSize: width * 0.07,
+                  color: Color(0xFFFFF4C7),
+                ),
               ),
             ],
           ),
