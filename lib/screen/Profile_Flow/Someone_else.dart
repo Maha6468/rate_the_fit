2a7417ddb33633ca5f9b7c513a3a213139f1f9e7
Maha6468 <motherma7633@gmail.com>
2026-03-07 12:17:07 +0600

    last update

--- lib/screen/Profile_Flow/Someone_else.dart
+++ lib/screen/Profile_Flow/Someone_else.dart
@@ -1,8 +1,8 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class Someone_else extends StatelessWidget {
+  const Someone_else({super.key});
 
   @override
   Widget build(BuildContext context) {
