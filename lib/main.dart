584374ab4c0d1dccc02d62a11a61d45a0d578f0d
Maha6468 <motherma7633@gmail.com>
2026-03-15 06:44:11 +0600

    last update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:rate_the_fit/coustom_widget/custom_routes.dart';
 import 'package:rate_the_fit/practice.dart';
 import 'package:rate_the_fit/screen/Side_Bar_Flow/side_bar.dart';
 import 'package:rate_the_fit/screen/home_flow/home.dart';
+import 'package:rate_the_fit/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -18,10 +19,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      initialRoute: "/",
-      routes: AppRoutes.routes,
+      // initialRoute: "/",
+      // routes: AppRoutes.routes,
       //home: Home(),
       //home: SideBar(),
+      home: Splash_Screen(),
     );
   }
 }
